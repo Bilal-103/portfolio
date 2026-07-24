352e8f217d73bb015a8bf0637b54bc810a2f5f76
Bilal-103 <bilalbutt341236@gmail.com>
2026-07-24 01:11:13 -0700

    icon

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite";//vite?
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));//func?
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],//dictionary in dictionary?
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),//resolve to what?
     },
   },
 });
+//what is vite? diff bw type scrpit vss java scri[t?]
